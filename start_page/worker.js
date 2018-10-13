cb0cb51ed8bddebe6db1c99b1d3a450301bb3e21
OligerMan <32018536+OligerMan@users.noreply.github.com>
2018-10-13 09:52:44 +0300

    Try to fix audio error

--- start_page/worker.js
+++ start_page/worker.js
@@ -1,6 +1,8 @@
-var audio_signal = new Audio();
-audio_signal.src = 'start_page/memas.mp3';
+//var audio_signal = new Audio();
+//audio_signal.src = 'start_page/memas.mp3';
+var c = document.createElement('audio'); 
+c.src='start_page/memas.mp3'; 
 
 onmessage = function(event){
-    audio_signal.play();
+    c.play();
 }
